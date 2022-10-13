e2e3c50c7229aa63b6a3eae998549db9ad8346c7
Jordan <jordancole2245@gmail.com>
2022-10-13 17:49:41 +0100

    added validation & passwords js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-'use strict'
+"use strict";
+
+let password = document.getElementById("password");
+let confirmPassword = document.getElementById("confirm-pass");
+
+function validatePassword() {
+  confirmPassword.setCustomValidity(
+    password.value != confirmPassword.value ? "Passwords Don't Match" : ""
+  );
+}
+
+console.log(password);
+
+password.onchange = validatePassword;
+confirmPassword.onkeyup = validatePassword;
